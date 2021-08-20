a59eb70b59edc3a3ded7f1aa6d874ebb124ec22d
Valentim Helio de Oliveira <valentimhelio@gmail.com>
2021-08-20 15:58:45 -0400

    Novas Implementações.

---
+++ Api.Data/Context/MyContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Api.Data.Context
+{
+    public class MyContext
+    {
+    }
+}
---
+++ Api.Domain/Entities/UserEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace Api.Domain.Entities
+{
+    public class UserEntity : BaseEntity
+    {
+        public string Nome { get; set; }
+
+        public string Email { get; set; }
+    }
+}
