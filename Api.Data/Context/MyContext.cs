284bb5089c62d4a700a3bb48ed558d47f84fee10
Valentim Helio de Oliveira <valentimhelio@gmail.com>
2021-08-24 10:49:13 -0400

    Alteração aula 55.

--- Api.Data/Context/MyContext.cs
+++ Api.Data/Context/MyContext.cs
@@ -1,10 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Api.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Api.Data.Context
 {
-    public class MyContext
+    public class MyContext : DbContext
     {
+        public DbSet<UserEntity> Users { get; set; }
+
+        public MyContext(DbContextOptions<MyContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuuilder)
+        {
+            base.OnModelCreating(modelBuuilder);
+        }
     }
 }
